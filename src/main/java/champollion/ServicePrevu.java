c09fc8365a615ccf8293647e32afd3a752435a6d
Camille <ca.carbasse@orange.fr>
2020-11-19 09:12:47 +0100

    gestion des conflits main/java/champollion

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 package champollion;
 
 public class ServicePrevu {
 	// TODO : implémenter cette classe
 
 }
-=======
-package champollion;
 
-public class ServicePrevu {
-	// TODO : implémenter cette classe
-
-}
->>>>>>> 2010826a10e64dca9b5a3cae5c7fa4c132016c50
